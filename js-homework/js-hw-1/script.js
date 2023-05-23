332c2e25d3a3272ae3eb87f7143534767959333d
HryhorchukRoma <124773242+HryhorchukRoma@users.noreply.github.com>
2023-05-23 13:22:26 +0300

    Add files via upload

---
+++ js-homework/js-hw-1/script.js
@@ -0,0 +1,74 @@
+// друге завдання
+
+// console.log('roma from script.js')
+
+
+
+//третє завдання
+
+// let a=1
+// let b=2
+// console.log(a, b)
+// b=a
+// console.log(a, b)
+
+
+
+//четверте завдання
+
+// let obj={
+//     String:'String',
+//     Number:2,
+//     Boolean:true,
+//     Undefined: undefined,
+//     Null: null
+// }
+
+
+
+//п'яте завдання
+
+// isAdult=confirm("Are you 18+?")
+// console.log(isAdult)
+
+
+
+//шосте завдання
+
+// let myName='roma'
+// let mySurname='hryhorchuk'
+// let myGroup=141
+// let myBirthYear=2005
+// let myFamily=confirm("Are you married?")
+// console.log(myGroup , typeof(myGroup))
+// console.log(myBirthYear , typeof(myBirthYear))
+// console.log( myFamily, typeof(myFamily))
+// console.log(myName , typeof(myName))
+// console.log(mySurname , typeof(mySurname))
+// let c=null
+// let d;
+// console.log(typeof(c), typeof(d))
+
+
+
+//сьоме завдання
+
+// let login=prompt("enter your login")
+// let password=prompt('enter your password')
+// let email=prompt("entewr your email")
+// let e='Dear ${login}, your email is ${email} and your password is ${password}'
+// alert(e)
+
+
+
+//восьме завдання
+
+// let answer;
+// let amount=prompt('Enter amount of seconds: ')
+// if(amount<60) answer=amount+" seconds";
+// else if(amount/60<60) answer=(amount/60) +" minutes";
+// else if(amount/3600<24) answer=(amount/3600) +" hours";
+// else if(amount/86400<=31) answer=(amount/86400) +" days";
+// else if(amount/2678400<12) answer=(amount/2678400) + ' months';
+// else if(amount/32140800) answer=(amount/32140800)+' years';;
+// alert(answer)
