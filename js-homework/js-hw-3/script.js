fa84abd1e6b48b2c2c235846e21eca97afda2f9c
HryhorchukRoma <124773242+HryhorchukRoma@users.noreply.github.com>
2023-06-06 18:45:52 +0300

    js-hw-3

--- js-homework/js-hw-3/script.js
+++ js-homework/js-hw-3/script.js
@@ -64,21 +64,48 @@
 
 //завдання п'ять
 
-const array = [5, 'Limit', 12, 'a', '3', 99, 2, [2, 4, 3, '33', 'a', 'text'], 'strong', 'broun']
-function sort(arr) {
-    let array=[...arr]
-    let numArr = [], strArr = [];
-    array.forEach(i => {
-        if (typeof (array[i] === Number)) numArr += array[i];
-        else if (typeof (array[i] === String)) strArr += array[i];
-        else if (typeof (array[i] === Object)) {
-            for (let j = 0; j < array[i].length; i++) {
-                if (typeof (array[i] === Number)) numArr += array[i][j];
-                else if (typeof (array[i] === String)) strArr += array[i][j];
-            }
-        }
+// const array = [5, 'Limit', 12, 'a', '3', 99, 2, [2, 4, 3, '33', 'a', 'text'], 'strong', 'broun']
+// const numArr=[], strArr=[];
+// function sort(arr){
+//     arr.forEach(item => {
+//         if(arr.isArray()) {sort(item)}
+//         if(typeOfitem== "object"){sort(item.values())}
+//         if(typeof item == "number"){numArr.push(item)}
+//         if(typeof item == "string"){strArr.push(item)}
+//     });
+// }
 
-    })
-    return numArr, strArr;
-}
-console.log(sort(array))
+
+//завдання шість
+
+// const a=parseInt(prompt("Enter a:")), 
+// b=parseInt(prompt("Enter b:")),
+// op=parseInt(prompt("Enter op:"));
+// let sum;
+// function calc(a, b, op){
+//   switch (op){
+//     case 1: sum=(a-b);
+//     break;
+//     case 2: sum=(a*b);
+//     break;
+//     case 3: sum=(a/b);
+//     break;
+//     default: sum=(a+b)
+//   }
+//   return sum;
+// }
+// console.log(calc(a, b, op))
+
+
+//завдання сьоме
+
+// const arr=[1,2,3,4,4,5]
+// function findUnique(arr){
+//     for (let i=0; i<arr.length; i++){
+//         for (let j=i+1; j<arr.length; j++){
+//             if(arr[i]===arr[j]) return false
+//         }
+//     }
+//     return true;
+// }
+// console.log(findUnique(arr))
