448b27641862eb95789f767e6a7428b14b6ae655
HryhorchukRoma <124773242+HryhorchukRoma@users.noreply.github.com>
2023-06-26 22:49:29 +0300

    commit

--- js-homework/js-hw-6/script.js
+++ js-homework/js-hw-6/script.js
@@ -26,6 +26,17 @@
 //     })
 // })
 
-
 //завдання два
 
+const first = document.querySelector("h1");
+const p = document.querySelectorAll("p");
+const list = document.querySelector("#myList");
+const invise = document.querySelector(".invise");
+
+first.style = "background-color: lime; width: 280px;";
+p[0].style = "font-weight: bold;";
+p[1].style = "color: red;";
+p[2].style = "text-decoration : underline;";
+p[3].style = "font-style : italic;";
+list.style = "display: flex; list-style: none;";
+invise.style = "opacity : 0;";
