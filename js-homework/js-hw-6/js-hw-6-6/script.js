764d3c69d77c4eceac96d87ef80b783741278641
HryhorchukRoma <124773242+HryhorchukRoma@users.noreply.github.com>
2023-06-27 17:37:08 +0300

    1

--- js-homework/js-hw-6/js-hw-6-6/script.js
+++ js-homework/js-hw-6/js-hw-6-6/script.js
@@ -8,6 +8,7 @@ const img = document.querySelectorAll(".shoe");
 const newtxt = document.querySelector(".new");
 const notnewtxt = document.querySelector(".notnew");
 const background = document.querySelectorAll(".gradient");
+const newSpan = document.querySelector('.new')
 
 let count = 0;
 
@@ -33,18 +34,55 @@ color.forEach((item, index) => {
     item.classList.add("active");
     switch (item.getAttribute("color")) {
       case "blue":
-        field.textContent = `${item.getAttribute("data-price") - 1}.99`;
+        field.textContent = `${Math.floor((item.getAttribute("data-price") - 1))}` + '.99';
       case "red":
-        field.textContent = `${item.getAttribute("data-price") - 1}.99`;
+        field.textContent = `${Math.floor((item.getAttribute("data-price") - 1))}` + '.99';
       case "green":
-        field.textContent = `${item.getAttribute("data-price") - 1}.99`;
+        field.textContent = `${Math.floor((item.getAttribute("data-price") - 1))}` + '.99';
       case "orange":
-        field.textContent = `${item.getAttribute("data-price") - 1}.99`;
+        field.textContent = `${Math.floor((item.getAttribute("data-price") - 1))}` + '.99';
       case "black":
-        field.textContent = `${item.getAttribute("data-price") - 1}.99`;
+        field.textContent = `${Math.floor((item.getAttribute("data-price") - 1))}` + '.99';
     }
   });
 });
+
+function renderPrice(x=1){
+  let i;
+  color.forEach((item, index) => {
+    if (item.classList.value == "color active") {
+      i = index;
+    }
+  });
+  switch (color[i].getAttribute("color")) {
+    case "blue":
+      field.textContent = `${Math.floor((color[i].getAttribute("data-price") - 1) * x)}` + '.99';
+    case "red":
+      field.textContent = `${Math.floor((color[i].getAttribute("data-price") - 1) * x)}` + '.99';
+    case "green":
+      field.textContent = `${Math.floor((color[i].getAttribute("data-price") - 1) * x)}` + '.99';
+    case "orange":
+      field.textContent = `${Math.floor((color[i].getAttribute("data-price") - 1) * x)}` + '.99';
+    case "black":
+      field.textContent = `${Math.floor((color[i].getAttribute("data-price") - 1) * x)}` + '.99';
+  }
+}
+let buff = false
+newSpan.addEventListener('click', () => {
+  if(buff){
+    newSpan.style.backgroundColor = '#2175f5';
+    buff = false
+    newSpan.textContent = 'new';
+    renderPrice()
+  }
+  else{
+    newSpan.style.backgroundColor = 'rgb(252, 74, 26)'
+    buff = true
+    newSpan.textContent = 'old';
+    renderPrice(0.7)
+  }
+})
+
 // function checkImg(i) {
 //   img.forEach((item) => {
 //     if (i.classList.value == "shoe show") {
